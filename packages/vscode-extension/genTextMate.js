03f0303386dd7f4805b111c4b6239208d9122f1a
Necla Akalin <166384019+nnecla@users.noreply.github.com>
2025-08-15 15:23:46 +0100

    test3

--- packages/vscode-extension/genTextMate.js
+++ packages/vscode-extension/genTextMate.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const languageSupport = require('@neo4j-cypher/language-support');
 const textMateGrammar = languageSupport.textMateGrammar;
 
-// test2
+// test3
 fs.writeFileSync(
   './syntaxes/cypher.json',
   JSON.stringify(textMateGrammar, undefined, 2),
